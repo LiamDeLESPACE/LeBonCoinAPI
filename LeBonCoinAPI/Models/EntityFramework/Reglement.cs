2fb10fd884f0a77d284558eea2355943c0478529
LiamDeLespace <liam.medkour@etu.univ-smb.fr>
2024-03-12 17:38:07 +0100

    reglement (sans jointure)

---
+++ LeBonCoinAPI/Models/EntityFramework/Reglement.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace LeBonCoinAPI.Models.EntityFramework
+{
+    [Table("t_j_reglement_rgl")]
+    public class Reglement
+    {
+
+        [Key]
+        [Column("rgl_numtransaction")]
+        [StringLength(50)]
+        public string? NumeroTransaction { get; set; }
+
+        [Key]
+        [Column("tyr_id")]
+        public int IdTypeReglement { get; set; }
+
+        [Key]
+        [Column("res_id")]
+        public int IdReservation { get; set; }
+    }
+}
