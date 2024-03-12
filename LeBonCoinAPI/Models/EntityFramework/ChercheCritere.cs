d79821a6aabd7e8315c6c7d0b5a06b84eaab1f23
rusafae <rusafae@D350-S15>
2024-03-12 17:35:25 +0100

    last class

---
+++ LeBonCoinAPI/Models/EntityFramework/ChercheCritere.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace LeBonCoinAPI.Models.EntityFramework
+{
+    [Table("t_j_cherchercritere_cri")]
+    public class ChercheCritere
+    {
+        [Key]
+        [Column("cri_idcritere")]
+        public int IdCritere { get; set; }
+
+        [Key]
+        [Column("cri_idrecherche")]
+        public int IdRecherche;
+
+    }
+}
---
+++ LeBonCoinAPI/Models/EntityFramework/Critere.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace LeBonCoinAPI.Models.EntityFramework
+{
+    [Table("t_e_critere_cri")]
+    public class Critere
+    {
+        [Key]
+        [Column("cri_idcritere")]
+        public int IdCritere { get; set; }
+
+        [Key]
+        [Column("cri_idcategoriecritere")]
+        public int IdCategorieCritere { get; set; }
+
+
+
+        [Required]
+        [Column("cri_libellecritere")]
+        public string libellecritere { get; set; } = null!;
+    }
+}
