01e20036b6f96e960e60d6f0a51a314d537c9d5b
rusafae <rusafae@D350-S15>
2024-03-12 11:42:56 +0100

    premier modele

---
+++ LeBonCoinAPI/Models/EntityFramework/Favoris.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace LeBonCoinAPI.Models.EntityFramework
+{
+    [Table("t_j_favoris_fav")]
+    public class Favoris
+    {
+        [Key]
+        [Column("fav_idprofil")]
+        public int Idprofil { get; set; }
+
+        [Key]
+        [Column("fav_idannonce")]
+        public int IdAnnonce;
+    }
+}
