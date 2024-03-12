6070be7c87ad37809127a8d8585a9a86bb858dba
LiamDeLespace <liam.medkour@etu.univ-smb.fr>
2024-03-12 11:44:55 +0100

    Formulaire (non fini) et action

---
+++ LeBonCoinAPI/Models/EntityFramework/Action.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Infrastructure;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace LeBonCoinAPI.Models.EntityFramework
+{
+    [Table("t_e_action_act")]
+    public class Action
+    {
+        public Action()
+        {
+            FormulairesChatbot = new HashSet<FormulaireChatbot>();
+        }
+        [Key]
+        [Column("act_id")]
+        public int ActionId { get; set; }
+
+        [Column("act_libelle")]
+        [StringLength(50)]
+        public string? Libelle { get; set; }
+
+        [InverseProperty(nameof(FormulaireChatbot.ActionFormulaire))]
+        public virtual ICollection<FormulaireChatbot> FormulairesChatbot { get; set; }
+
+    }
+}
