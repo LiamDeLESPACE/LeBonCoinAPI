e0ccc673079b7595f82c99d5c5720fe181e07fc8
Nicolas Tochon <Nicolas.Tochon@etu.univ-smb.fr>
2024-03-12 16:40:32 +0100

    Avis

--- LeBonCoinAPI/Models/EntityFramework/avis.cs
+++ LeBonCoinAPI/Models/EntityFramework/avis.cs
@@ -5,28 +5,28 @@ using System.ComponentModel.DataAnnotations;
 namespace LeBonCoinAPI.Models.EntityFramework
 {
     [Table("t_e_avis_avi")]
-    public class avis
+    public class Avis
     {
         [Key]
-        [Column("avi_idavis")]
+        [Column("avi_id")]
         public int IdAvis { get; set; }
 
         [Key]
-        [Column("avi_idprofil")]
+        [Column("pro_id")]
         public int IdProfil { get; set; }
 
 
 
         [StringLength(100)]
         [Column("avi_titre")]
-        public string TitreAvis { get; set; }
+        public string? Titre { get; set; }
 
 
         [Column("avi_description")]
-        public string DescriptionAvis { get; set; }
+        public string? DescriptionAvis { get; set; }
 
         [Required]
         [Column("avi_note")]
-        public int NoteAvis { get; set; }
+        public int Note { get; set; }
     }
 }
