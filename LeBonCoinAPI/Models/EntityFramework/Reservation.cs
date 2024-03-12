a579350c966f36a56e1d3fd1269221efd0f49be9
Nicolas Tochon <Nicolas.Tochon@etu.univ-smb.fr>
2024-03-12 17:24:41 +0100

    création table incident et reservation

---
+++ LeBonCoinAPI/Models/EntityFramework/Reservation.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace LeBonCoinAPI.Models.EntityFramework
+{
+    [Table("t_e_reservation_res")]
+    public class Reservation
+    {
+        [Key]
+        [Column("res_id")]
+        public int IdReservation { get; set; }
+
+        [Key]
+        [Column("ann_id")]
+        public int IdAnnonce { get; set; }
+
+        [Key]
+        [Column("loc_id")]
+        public int IdLocataire { get; set; }
+
+        [Required]
+        [Column("res_datearrivee")]
+        public DateTime DateArrivee { get; set; }
+
+        [Required]
+        [Column("res_datedepart")]
+        public DateTime DateDepart { get; set; }
+
+        [Required]
+        [Column("res_nombrevoyageur")]
+        public int NombreVoyageur { get; set; }
+    }
+}
