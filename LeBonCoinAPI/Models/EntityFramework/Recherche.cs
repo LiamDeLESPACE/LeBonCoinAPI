7014099fa5b9c1fb4f76cb9aa4e36ce33a74b24c
Nicolas Tochon <Nicolas.Tochon@etu.univ-smb.fr>
2024-03-13 10:07:12 +0100

    liens recherche/chercheCritere, critere/chercheCritere et modif categoriecritere

--- LeBonCoinAPI/Models/EntityFramework/Recherche.cs
+++ LeBonCoinAPI/Models/EntityFramework/Recherche.cs
@@ -1,12 +1,18 @@
 ﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.ComponentModel.DataAnnotations;
+using Microsoft.EntityFrameworkCore.Infrastructure;
 
 namespace LeBonCoinAPI.Models.EntityFramework
 {
     [Table("t_j_recherche_rch")]
     public class Recherche
     {
+        public Recherche()
+        {
+            ChercheCritereDeLaCherche = new HashSet<ChercheCritere>();
+        }
+
         [Key]
         [Column("rch_id")]
         public int IdRecherche { get; set; }
@@ -25,5 +31,9 @@ namespace LeBonCoinAPI.Models.EntityFramework
         [Column("vil_codeinsee")]
         [RegularExpression(@"^[0-9]{5}$", ErrorMessage = "Le code insee doit contenir 5 chiffres")]
         public string CodeInsee { get; set; } = null!;
+
+        [InverseProperty(nameof(ChercheCritere.RechercheDuCritereCherche))]
+        public virtual ICollection<ChercheCritere> ChercheCritereDeLaCherche { get; set; }
+
     }
 }
