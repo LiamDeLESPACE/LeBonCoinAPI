d8c831346fa37ef9de93a1992f0bb3541517cc5a
Nicolas Tochon <Nicolas.Tochon@etu.univ-smb.fr>
2024-03-13 10:14:31 +0100

    lien Ville/Departement

--- LeBonCoinAPI/Models/EntityFramework/Departement.cs
+++ LeBonCoinAPI/Models/EntityFramework/Departement.cs
@@ -1,12 +1,18 @@
 ﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.ComponentModel.DataAnnotations;
+using Microsoft.EntityFrameworkCore.Infrastructure;
 
 namespace LeBonCoinAPI.Models.EntityFramework
 {
     [Table("t_e_departement_dep")]
     public class Departement
     {
+        public Departement()
+        {
+            VillesDepartement = new HashSet<Ville>();
+        }
+
         [Key]
         [Column("dep_id")]
         public int IdDepartement { get; set; }
@@ -20,5 +26,9 @@ namespace LeBonCoinAPI.Models.EntityFramework
         [Column("dep_nom")]
         [StringLength(50)]
         public string Nom { get; set; } = null!;
+
+        [InverseProperty(nameof(Ville.DepartementDeLaVille))]
+        public virtual ICollection<Ville> VillesDepartement { get; set; }
+
     }
 }
