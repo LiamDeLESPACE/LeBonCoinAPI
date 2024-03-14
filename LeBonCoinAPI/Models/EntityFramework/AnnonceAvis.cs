0db9f71598dbac344f9cbfa4dd1cf8b5183251d6
LiamDeLespace <liam.medkour@etu.univ-smb.fr>
2024-03-14 10:25:31 +0100

    Liaison annonceavis avis

--- LeBonCoinAPI/Models/EntityFramework/AnnonceAvis.cs
+++ LeBonCoinAPI/Models/EntityFramework/AnnonceAvis.cs
@@ -15,9 +15,15 @@ namespace LeBonCoinAPI.Models.EntityFramework
         [Column("ann_id")]
         public int AnnonceId;
 
+        //Annonce lié
         [ForeignKey(nameof(AnnonceId))]
         [InverseProperty(nameof(Annonce.AvisAnnonce))]
         public virtual Annonce AnnonceAvi { get; set; } = null!;
+
+        //Avis lié
+        [ForeignKey(nameof(AvisId))]
+        [InverseProperty(nameof(Avis.AvisTypeAnnonceAvis))]
+        public virtual Avis AviAvisAnnonce { get; set; } = null!;
 
     }
 }
