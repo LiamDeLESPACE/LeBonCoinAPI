928f939987039ba90b4ce82bd9fa9f3523941e68
yves <dorian.papiris@etu.univ-smb.fr>
2024-03-19 08:42:33 +0100

    userContrller

---
+++ LeBonCoinAPI/Controllers/UserController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using LeBonCoinAPI.Models;
+using LeBonCoinAPI.Models.Auth;
+
+namespace LeBonCoinAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        [HttpGet]
+        [Route("GetUserData")]
+        [Authorize(Policy = Policies.User)]
+        public IActionResult GetUserData()
+        {
+            return Ok("This is a response from user method");
+        }
+        
+    }
+}
