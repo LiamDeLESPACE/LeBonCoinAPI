59f5276c6d3928d19a827da65c8bd9f8aed930f7
LiamDeLespace <liam.medkour@etu.univ-smb.fr>
2024-03-20 18:00:45 +0100

    Entreprise et SecteurActivite

--- LeBonCoinAPI/Models/EntityFramework/SecteurActivite.cs
+++ LeBonCoinAPI/Models/EntityFramework/SecteurActivite.cs
@@ -1,7 +1,27 @@
-﻿namespace LeBonCoinAPI.Models.EntityFramework
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace LeBonCoinAPI.Models.EntityFramework
 {
+    [Table("t_e_secteuractivite_sct")]
     public class SecteurActivite
     {
+        public SecteurActivite()
+        {
+            EntreprisesSecteurActivite = new HashSet<Entreprise>();
+        }
+        [Key]
+        [Column("sct_id")]
+        public int SecteurId { get; set; }
 
+        [Required]
+        [Column("sct_nomsecteur")]
+        public string NomSecteur { get; set; }
+
+        //Entreprises
+        [InverseProperty(nameof(Entreprise.SecteurEntreprise))]
+        public virtual ICollection<Entreprise> EntreprisesSecteurActivite{ get; set; }
     }
 }
