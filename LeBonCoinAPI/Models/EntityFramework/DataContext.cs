f184b4959798cae0be72f11fbeb59c40cd7296db
LiamDeLespace <liam.medkour@etu.univ-smb.fr>
2024-03-21 14:53:08 +0100

    Datacontext initialisation

--- LeBonCoinAPI/Models/EntityFramework/DataContext.cs
+++ LeBonCoinAPI/Models/EntityFramework/DataContext.cs
@@ -1,28 +1,48 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.VisualBasic;
+using System.Runtime.Intrinsics.X86;
 
 namespace LeBonCoinAPI.Models.EntityFramework
 {
     public partial class DataContext : DbContext
     {
-        public DataContext()
-        {
-            
-        }
-        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+        public DataContext() { }
+        public DataContext(DbContextOptions<DataContext> options) 
+            : base(options) { }
 
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            if (!optionsBuilder.IsConfigured)
-            {
-                //To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseNpgsql("DBContext");
-            }
-        }
+        public virtual DbSet<Admin> Admins { get; set; } = null!;
+        public virtual DbSet<Adresse> Adresses { get; set; } = null!;
+        public virtual DbSet<Annonce> Annonces { get; set; } = null!;
+        public virtual DbSet<Commentaire> Commentaires { get; set; } = null!;
+        public virtual DbSet<CarteBancaire> CarteBancaires { get; set; } = null!;
+        public virtual DbSet<Departement> Departements { get; set; } = null!;
+        public virtual DbSet<Entreprise> Entreprises { get; set; } = null!;
+        public virtual DbSet<Equipement> Equipements { get; set; } = null!;
+        public virtual DbSet<Favoris> lesFavoris { get; set; } = null!;
+        public virtual DbSet<Particulier> Particuliers { get; set; } = null!;
+        public virtual DbSet<Photo> Photos { get; set; } = null!;
+        public virtual DbSet<PossedeEquipement> PossedeEquipements { get; set; } = null!;
+        public virtual DbSet<Profil> Profils { get; set; } = null!;
+        public virtual DbSet<Reglement> Reglements { get; set; } = null!;
+        public virtual DbSet<Reservation> Reservations { get; set; } = null!;
+        public virtual DbSet<SecteurActivite> SecteurActivites { get; set; } = null!;
+        public virtual DbSet<Signale> Signales { get; set; } = null!;
+        public virtual DbSet<TypeLogement> TypeLogements { get; set; } = null!;
+        public virtual DbSet<TypeEquipement> TypeEquipements { get; set; } = null!;
+        public virtual DbSet<Ville> Villes { get; set; } = null!;
+
+        //        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        //        {
+        //            if (!optionsBuilder.IsConfigured)
+        //            {
+        //#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+        //                optionsBuilder.UseNpgsql("Server=localhost;port=5432;Database=FilmRatingsDB; uid=postgres; password=postgres;");
+        //            }
+        //        }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-         
+           
 
             OnModelCreatingPartial(modelBuilder);
         }
