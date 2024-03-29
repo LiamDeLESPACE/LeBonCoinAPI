8442910b4dd935f06aea339eea022829e3eaac96
LiamDeLespace <liam.medkour@etu.univ-smb.fr>
2024-03-29 14:19:19 +0100

    AdressesController tests finis

--- LeBonCoinAPITests/Controllers/AdressesControllerTests.cs
+++ LeBonCoinAPITests/Controllers/AdressesControllerTests.cs
@@ -10,6 +10,8 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Mvc;
 using Moq;
 using Azure;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+using System.Data;
 
 namespace LeBonCoinAPI.Controllers.Tests
 {
@@ -18,31 +20,35 @@ namespace LeBonCoinAPI.Controllers.Tests
     {
         private AdressesController _controller;
 
-        private DataContext _context;
+        private Mock<DataContext> _context;
 
-        /*var testListe = new List<Adresse>();
-            testListe.Add(new Adresse { CodeInsee = "74002", Rue = "Chemin de Cret-Vial", Numero = 47 });
-            testListe.Add(new Adresse { CodeInsee = "74002", Rue = "Rue de la capetaz", Numero = 1 });
-            testListe.Add(new Adresse { CodeInsee = "74002", Rue = "Chemin de Cret-Vial", Numero = 47 });
-            testListe.Add(new Adresse { CodeInsee = "01431", Rue = "Rue de Cornier", Numero = 15 });*/
+        //Arrange
+        Adresse adresse;
+        List<Adresse> testListe;
 
-        public AdressesControllerTests()
+public AdressesControllerTests()
         {
             var builder = new DbContextOptionsBuilder<DataContext>().UseNpgsql("Server=localhost; port=5432; Database=LeBonCoinSAE; uid=postgres; password=postgres;");
-            _context = new DataContext(builder.Options);
-            _controller = new AdressesController(_context);
+            _context = new Mock<DataContext>();
+            _controller = new AdressesController(_context.Object);
+        }
+
+        [TestInitialize]
+        public void InitialisationDesTests()
+        {
+            // Rajouter les initialisations exécutées avant chaque test
+             adresse = new Adresse { AdresseId = 1, CodeInsee = "74002", Rue = "Rue de Cornier", Numero = 15 };
+
+             testListe = new List<Adresse>();
+             testListe.Add(new Adresse { CodeInsee = "74002", Rue = "Chemin de Cret-Vial", Numero = 47 });
+             testListe.Add(new Adresse { CodeInsee = "74002", Rue = "Rue de la capetaz", Numero = 1 });
+             
+             
         }
 
         [TestMethod()]
         public void GetAdresse_ExistingIdPassed_ReturnsRightItem()
         {
-            //Arrange
-            Adresse adresse = new Adresse
-            {
-                CodeInsee = "01431",
-                Rue = "Rue de Cornier",
-                Numero = 15
-            };
 
             //Act
             var result = _controller.GetAdresse(1);
@@ -73,10 +79,6 @@ namespace LeBonCoinAPI.Controllers.Tests
         [TestMethod()]
         public void GetAdresses_ReturnsRightItems()
         {
-            //Arrange
-            List<Adresse> testListe = new List<Adresse>();
-            testListe.Add(new Adresse { CodeInsee = "74002", Rue = "Chemin de Cret-Vial", Numero = 47 });
-            testListe.Add(new Adresse { CodeInsee = "74002", Rue = "Rue de la capetaz", Numero = 1 });
 
             //Act
             var result = _controller.GetAdresses();
@@ -92,40 +94,25 @@ namespace LeBonCoinAPI.Controllers.Tests
         [TestMethod()]
         public void PostAdresse_ModelValidated_CreationOK()
         {
-            Adresse adresse1 = new Adresse
-            {
-                CodeInsee = "01431",
-                Rue = "Rue de Cornier",
-                Numero = 15
-
-            };
 
             //Act
-            var result = _controller.PostAdresse(adresse1).Result;
+            var result = _controller.PostAdresse(adresse).Result;
 
             //Assert
             Assert.IsInstanceOfType(result, typeof(ActionResult<Adresse>), "Pas un ActionResult");
             Assert.IsInstanceOfType(result.Result, typeof(CreatedAtActionResult), "Pas un CreatedAtActionResult");
             var actionResult = result.Result as CreatedAtActionResult;
             Assert.IsInstanceOfType(actionResult.Value, typeof(Adresse), "Pas une adresse");
-            adresse1.CodeInsee = ((Adresse)actionResult.Value).CodeInsee;
-            Assert.AreEqual(adresse1, (Adresse)actionResult.Value, "Adresses pas identiques");
+            adresse.CodeInsee = ((Adresse)actionResult.Value).CodeInsee;
+            Assert.AreEqual(adresse, (Adresse)actionResult.Value, "Adresses pas identiques");
 
         }
         [TestMethod()]
         public void PostAdresse_CodeInsee_CreationFailed()
         {
-            //Arrange
-            Adresse adresse1 = new Adresse
-            {
-                CodeInsee = "01431",
-                Rue = "Rue de Cornier",
-                Numero = 15
-
-            };
 
             //Act
-            var result = _controller.PostAdresse(adresse1).Result;
+            var result = _controller.PostAdresse(adresse).Result;
 
             //Assert
             Assert.IsNotInstanceOfType(result.Result, typeof(Adresse), "Pas un CreatedAtActionResult");
@@ -136,15 +123,7 @@ namespace LeBonCoinAPI.Controllers.Tests
         public async Task Put_WithInvalidId_ReturnsBadRequest()
         {
             // Arrange
-            var mockContext = new Mock<DataContext>();
-            var controller = new AdressesController(mockContext.Object);
-            int id = 1;
-            var adresse = new Adresse { 
-                AdresseId = 2,
-                CodeInsee = "01431",
-                Rue = "Rue de Cornier",
-                Numero = 15
-            }; // Suppose une adresse avec ID différent
+            int id = 2;//Mauvais ID
 
             // Act
             var result = await _controller.PutAdresse(id, adresse);
@@ -156,14 +135,11 @@ namespace LeBonCoinAPI.Controllers.Tests
         [TestMethod()]
         public async Task Put_WithValidId_ReturnsNoContent()
         {
-            // Arrange
-            var mockContext = new Mock<DataContext>();
-            var controller = new AdressesController(mockContext.Object);
-            int id = 1;
-            var adresse = new Adresse { AdresseId = 1 }; // Suppose une adresse avec ID correspondant
+
+            int id = 1; //BonID
 
             // Act
-            var result = await controller.PutAdresse(id, adresse);
+            var result = await _controller.PutAdresse(id, adresse);
 
             // Assert
             Assert.IsInstanceOfType(result,typeof(NoContentResult));
@@ -173,6 +149,20 @@ namespace LeBonCoinAPI.Controllers.Tests
         public void DeleteAdresseTest()
         {
 
+            //Act
+            var result = _controller.GetAdresse(1);
+
+            //Existence
+            var actionResult = result.Result as ActionResult<Adresse>;
+            Assert.IsNotNull(actionResult.Value, "Valeur nulle");
+            Assert.IsInstanceOfType(actionResult.Value, typeof(Adresse), "Pas une adresse");
+
+            //Act
+            var resultDest = _controller.DeleteAdresse(1);
+
+            //Assert
+            Assert.IsInstanceOfType(resultDest.Result, typeof(ActionResult<Adresse>), "Pas un ActionResult");
+            Assert.IsNull(resultDest.Result, "Adresse pas null");
         }
     }
 }
