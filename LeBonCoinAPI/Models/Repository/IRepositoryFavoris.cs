88643aaba017a6bef2d765db3fe2e9fcc269d3da
Nicolas Tochon <Nicolas.Tochon@etu.univ-smb.fr>
2024-04-03 10:54:00 +0200

    repository manager Favoris

---
+++ LeBonCoinAPI/Models/Repository/IRepositoryFavoris.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LeBonCoinAPI.Models.Repository
+{
+    public interface IRepositoryFavoris<TEntity>
+    {
+        ActionResult<IEnumerable<TEntity>> GetAll();
+        ActionResult<TEntity> GetByIds(int idProfil, int idAnnonce);
+        ActionResult<TEntity> GetByIdProfil(int idProfil);
+        void Add(TEntity entity);
+        void Update(TEntity entityToUpdate, TEntity entity);
+        void Delete(TEntity entity);
+    }
+}
