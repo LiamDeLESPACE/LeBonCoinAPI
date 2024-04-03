897b5e9e7dfde171600ba876610f10021cdb1821
Nicolas Tochon <Nicolas.Tochon@etu.univ-smb.fr>
2024-04-03 10:38:42 +0200

    repository manager Entreprise

---
+++ LeBonCoinAPI/DataManager/EntrepriseManager.cs
@@ -0,0 +1,49 @@
+﻿using LeBonCoinAPI.Models.EntityFramework;
+using LeBonCoinAPI.Models.Repository;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace LeBonCoinAPI.DataManager
+{
+    public class EntrepriseManager : IRepositoryEntreprise<Entreprise>
+    {
+        readonly DataContext? dataContext;
+        public EntrepriseManager() { }
+        public EntrepriseManager(DataContext context)
+        {
+            dataContext = context;
+        }
+        public ActionResult<IEnumerable<Entreprise>> GetAll()
+        {
+            return dataContext.Entreprises.ToList();
+        }
+
+        public ActionResult<Entreprise> GetById(int id)
+        {
+            return dataContext.Entreprises.FirstOrDefault(u => u.ProfilId == id);
+        }
+        public void Add(Entreprise entity)
+        {
+            dataContext.Entreprises.Add(entity);
+            dataContext.SaveChanges();
+        }
+        public void Update(Entreprise entreprise, Entreprise entity)
+        {
+            dataContext.Entry(entreprise).State = EntityState.Modified;
+            entreprise.ProfilId = entity.ProfilId;
+            entreprise.HashMotDePasse = entity.HashMotDePasse;
+            entreprise.Telephone = entity.Telephone;
+            entreprise.SecteurId = entity.SecteurId;
+            entreprise.Siret = entity.Siret;
+            entreprise.AdresseId = entity.AdresseId;
+            entreprise.Nom = entity.Nom;
+            
+            dataContext.SaveChanges();
+        }
+        public void Delete(Entreprise entreprise)
+        {
+            dataContext.Entreprises.Remove(entreprise);
+            dataContext.SaveChanges();
+        }
+    }
+}
