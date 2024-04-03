ae09729998a13045a7f2fd3b7dc3963618585806
Nicolas Tochon <Nicolas.Tochon@etu.univ-smb.fr>
2024-04-03 11:28:48 +0200

    repository manager PossedeEquipement

---
+++ LeBonCoinAPI/DataManager/PossedeEquipementManager.cs
@@ -0,0 +1,53 @@
+﻿using LeBonCoinAPI.Models.EntityFramework;
+using LeBonCoinAPI.Models.Repository;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace LeBonCoinAPI.DataManager
+{
+    public class PossedeEquipementManager : IRepositoryPossedeEquipement<PossedeEquipement>
+    {
+        readonly DataContext? dataContext;
+        public PossedeEquipementManager() { }
+        public PossedeEquipementManager(DataContext context)
+        {
+            dataContext = context;
+        }
+        public ActionResult<IEnumerable<PossedeEquipement>> GetAll()
+        {
+            return dataContext.PossedeEquipements.ToList();
+        }
+
+        public ActionResult<PossedeEquipement> GetByIds(int idAnnonce, int idEquipement)
+        {
+            return dataContext.PossedeEquipements.FirstOrDefault(c => c.AnnonceId == idAnnonce && c.EquipementId == idEquipement);
+        }
+
+        public ActionResult<PossedeEquipement> GetByIdAnnonce(int idAnnonce)
+        {
+            return dataContext.PossedeEquipements.FirstOrDefault(u => u.AnnonceId == idAnnonce);
+        }
+        public ActionResult<PossedeEquipement> GetByIdEquipement(int idEquipement)
+        {
+            return dataContext.PossedeEquipements.FirstOrDefault(u => u.EquipementId == idEquipement);
+        }
+        public void Add(PossedeEquipement entity)
+        {
+            dataContext.PossedeEquipements.Add(entity);
+            dataContext.SaveChanges();
+        }
+        public void Update(PossedeEquipement possedeEquipement, PossedeEquipement entity)
+        {
+            dataContext.Entry(possedeEquipement).State = EntityState.Modified;
+            possedeEquipement.AnnonceId = entity.AnnonceId;
+            possedeEquipement.EquipementId = entity.EquipementId;
+
+            dataContext.SaveChanges();
+        }
+        public void Delete(PossedeEquipement possedeEquipement)
+        {
+            dataContext.PossedeEquipements.Remove(possedeEquipement);
+            dataContext.SaveChanges();
+        }
+    }
+}
---
+++ LeBonCoinAPI/Models/Repository/IRepositoryPossedeEquipement.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LeBonCoinAPI.Models.Repository
+{
+    public interface IRepositoryPossedeEquipement<TEntity>
+    {
+        ActionResult<IEnumerable<TEntity>> GetAll();
+        ActionResult<TEntity> GetByIds(int idAnnonce, int idEquipement);
+        ActionResult<TEntity> GetByIdAnnonce(int idAnnonce);
+        ActionResult<TEntity> GetByIdEquipement(int idEquipement);
+        void Add(TEntity entity);
+        void Update(TEntity entityToUpdate, TEntity entity);
+        void Delete(TEntity entity);
+    }
+}
