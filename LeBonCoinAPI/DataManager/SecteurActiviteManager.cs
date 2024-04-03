452449f026b9a9cb8a2296634a782d2385b3cd04
Nicolas Tochon <Nicolas.Tochon@etu.univ-smb.fr>
2024-04-03 14:14:36 +0200

    repository manager SecteurActivite

---
+++ LeBonCoinAPI/DataManager/SecteurActiviteManager.cs
@@ -0,0 +1,45 @@
+﻿using LeBonCoinAPI.Models.EntityFramework;
+using LeBonCoinAPI.Models.Repository;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace LeBonCoinAPI.DataManager
+{
+    public class SecteurActiviteManager : IRepositorySecteurActivite<SecteurActivite>
+    {
+        readonly DataContext? dataContext;
+        public SecteurActiviteManager() { }
+        public SecteurActiviteManager(DataContext context)
+        {
+            dataContext = context;
+        }
+        public ActionResult<IEnumerable<SecteurActivite>> GetAll()
+        {
+            return dataContext.SecteurActivites.ToList();
+        }
+
+        public ActionResult<SecteurActivite> GetById(int id)
+        {
+            return dataContext.SecteurActivites.FirstOrDefault(u => u.SecteurId == id);
+        }
+        public void Add(SecteurActivite entity)
+        {
+            dataContext.SecteurActivites.Add(entity);
+            dataContext.SaveChanges();
+        }
+        public void Update(SecteurActivite secteurActivite, SecteurActivite entity)
+        {
+            dataContext.Entry(secteurActivite).State = EntityState.Modified;
+            secteurActivite.SecteurId = entity.SecteurId;
+            secteurActivite.NomSecteur = entity.NomSecteur;
+            secteurActivite.EntreprisesSecteurActivite = entity.EntreprisesSecteurActivite;
+            
+            dataContext.SaveChanges();
+        }
+        public void Delete(SecteurActivite secteurActivite)
+        {
+            dataContext.SecteurActivites.Remove(secteurActivite);
+            dataContext.SaveChanges();
+        }
+    }
+}
