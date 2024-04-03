628482f0e1efe6700b22d1573e4ee2ce88f72835
Nicolas Tochon <Nicolas.Tochon@etu.univ-smb.fr>
2024-04-03 14:26:29 +0200

    repository manager TypeLogement

---
+++ LeBonCoinAPI/DataManager/TypeLogementManager.cs
@@ -0,0 +1,45 @@
+﻿using LeBonCoinAPI.Models.EntityFramework;
+using LeBonCoinAPI.Models.Repository;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace LeBonCoinAPI.DataManager
+{
+    public class TypeLogementManager : IRepositoryTypeLogement<TypeLogement>
+    {
+        readonly DataContext? dataContext;
+        public TypeLogementManager() { }
+        public TypeLogementManager(DataContext context)
+        {
+            dataContext = context;
+        }
+        public ActionResult<IEnumerable<TypeLogement>> GetAll()
+        {
+            return dataContext.TypeLogements.ToList();
+        }
+
+        public ActionResult<TypeLogement> GetById(int id)
+        {
+            return dataContext.TypeLogements.FirstOrDefault(u => u.TypeLogementId == id);
+        }
+        public void Add(TypeLogement entity)
+        {
+            dataContext.TypeLogements.Add(entity);
+            dataContext.SaveChanges();
+        }
+        public void Update(TypeLogement typeLogement, TypeLogement entity)
+        {
+            dataContext.Entry(typeLogement).State = EntityState.Modified;
+            typeLogement.TypeLogementId = entity.TypeLogementId;
+            typeLogement.Nom = entity.Nom;
+            typeLogement.AnnoncesTypeLogement = entity.AnnoncesTypeLogement;
+
+            dataContext.SaveChanges();
+        }
+        public void Delete(TypeLogement typeLogement)
+        {
+            dataContext.TypeLogements.Remove(typeLogement);
+            dataContext.SaveChanges();
+        }
+    }
+}
