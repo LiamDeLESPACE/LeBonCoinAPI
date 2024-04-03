fd0e460663a957b61b782054a5f7b4d713cc9f2f
Nicolas Tochon <Nicolas.Tochon@etu.univ-smb.fr>
2024-04-03 11:12:26 +0200

    repository manager Photo

---
+++ LeBonCoinAPI/Models/Repository/IRepositoryPhoto.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LeBonCoinAPI.Models.Repository
+{
+    public interface IRepositoryPhoto<TEntity>
+    {
+        ActionResult<IEnumerable<TEntity>> GetAll();
+        ActionResult<TEntity> GetById(int id);
+        ActionResult<TEntity> GetByIdProfil(int idProfil);
+        ActionResult<TEntity> GetByIdAnnonce(int idAnnonce);
+        void Add(TEntity entity);
+        void Update(TEntity entityToUpdate, TEntity entity);
+        void Delete(TEntity entity);
+    }
+}
