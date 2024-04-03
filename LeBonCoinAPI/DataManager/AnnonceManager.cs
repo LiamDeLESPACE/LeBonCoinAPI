3232d7434cb2d88fc400266bd2c129f829ec88e0
Nicolas Tochon <Nicolas.Tochon@etu.univ-smb.fr>
2024-04-03 09:06:50 +0200

    repository et manager Annonce

---
+++ LeBonCoinAPI/DataManager/AnnonceManager.cs
@@ -0,0 +1,60 @@
+﻿using LeBonCoinAPI.Models.EntityFramework;
+using LeBonCoinAPI.Models.Repository;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace LeBonCoinAPI.DataManager
+{
+    public class AnnonceManager : IRepositoryAnnonce<Annonce>
+    {
+        readonly DataContext? dataContext;
+        public AnnonceManager() { }
+        public AnnonceManager(DataContext context)
+        {
+            dataContext = context;
+        }
+        public ActionResult<IEnumerable<Annonce>> GetAll()
+        {
+            return dataContext.Annonces.ToList();
+        }
+
+        public ActionResult<Annonce> GetById(int id)
+        {
+            return dataContext.Annonces.FirstOrDefault(u => u.AnnonceId == id);
+        }
+        public void Add(Annonce entity)
+        {
+            dataContext.Annonces.Add(entity);
+            dataContext.SaveChanges();
+        }
+        public void Update(Annonce annonce, Annonce entity)
+        {
+            dataContext.Entry(annonce).State = EntityState.Modified;
+            annonce.AnnonceId = entity.AnnonceId;
+            annonce.AdresseId = entity.AdresseId;
+            annonce.TypeLogementId = entity.TypeLogementId;
+            annonce.ProfilId = entity.ProfilId;
+            annonce.Titre = entity.Titre;
+            annonce.DureeMinimumSejour = entity.DureeMinimumSejour;
+            annonce.Active = entity.Active;
+            annonce.DatePublication = entity.DatePublication;
+            annonce.Description = entity.Description;
+            annonce.Etoile = entity.Etoile;
+            annonce.NombrePersonnesMax = entity.NombrePersonnesMax;
+            annonce.PrixParNuit = entity.PrixParNuit;
+            annonce.NombreChambres = entity.NombreChambres;
+            annonce.EquipementsPossedesAnnonce = entity.EquipementsPossedesAnnonce;
+            annonce.ReservationsAnnonce = entity.ReservationsAnnonce;
+            annonce.SignalementsAnnonce = entity.SignalementsAnnonce;
+            annonce.FavorisAnnonce = entity.FavorisAnnonce;
+            annonce.PhotosAnnonce = entity.PhotosAnnonce;
+
+            dataContext.SaveChanges();
+        }
+        public void Delete(Annonce annonce)
+        {
+            dataContext.Annonces.Remove(annonce);
+            dataContext.SaveChanges();
+        }
+    }
+}
