b775a935a13c72c36607edec926491f387c202d1
Nicolas Tochon <Nicolas.Tochon@etu.univ-smb.fr>
2024-04-03 14:19:33 +0200

    repository manager signale

---
+++ LeBonCoinAPI/DataManager/SignaleManager.cs
@@ -0,0 +1,54 @@
+﻿using LeBonCoinAPI.Models.EntityFramework;
+using LeBonCoinAPI.Models.Repository;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace LeBonCoinAPI.DataManager
+{
+    public class SignaleManager : IRepositorySignale<Signale>
+    {
+        readonly DataContext? dataContext;
+        public SignaleManager() { }
+        public SignaleManager(DataContext context)
+        {
+            dataContext = context;
+        }
+        public ActionResult<IEnumerable<Signale>> GetAll()
+        {
+            return dataContext.Signales.ToList();
+        }
+
+        public ActionResult<Signale> GetByIds(int idAnnonce, int idProfil)
+        {
+            return dataContext.Signales.FirstOrDefault(c => c.AnnonceId == idAnnonce && c.ProfilId == idProfil);
+        }
+        public ActionResult<Signale> GetByIdProfil(int idProfil)
+        {
+            return dataContext.Signales.FirstOrDefault(u => u.ProfilId == idProfil);
+        }
+
+        public ActionResult<Signale> GetByIdAnnonce(int idAnnonce)
+        {
+            return dataContext.Signales.FirstOrDefault(u => u.AnnonceId == idAnnonce);
+        }
+
+        public void Add(Signale entity)
+        {
+            dataContext.Signales.Add(entity);
+            dataContext.SaveChanges();
+        }
+        public void Update(Signale signale, Signale entity)
+        {
+            dataContext.Entry(signale).State = EntityState.Modified;
+            signale.ProfilId = entity.ProfilId;
+            signale.AnnonceId = entity.AnnonceId;
+            
+            dataContext.SaveChanges();
+        }
+        public void Delete(Signale signale)
+        {
+            dataContext.Signales.Remove(signale);
+            dataContext.SaveChanges();
+        }
+    }
+}
---
+++ LeBonCoinAPI/Models/Repository/IRepositorySignale.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LeBonCoinAPI.Models.Repository
+{
+    public interface IRepositorySignale<TEntity>
+    {
+        ActionResult<IEnumerable<TEntity>> GetAll();
+        ActionResult<TEntity> GetByIds(int idAnnonce, int idProfil);
+        ActionResult<TEntity> GetByIdProfil(int idProfil);
+        ActionResult<TEntity> GetByIdAnnonce(int idAnnonce);
+        void Add(TEntity entity);
+        void Update(TEntity entityToUpdate, TEntity entity);
+        void Delete(TEntity entity);
+    }
+}
