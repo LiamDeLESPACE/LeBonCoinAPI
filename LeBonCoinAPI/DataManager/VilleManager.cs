4c73209ef27d26930e2ff221a65faf75c88d0143
Nicolas Tochon <Nicolas.Tochon@etu.univ-smb.fr>
2024-04-03 14:30:59 +0200

    repository manager Ville

---
+++ LeBonCoinAPI/DataManager/VilleManager.cs
@@ -0,0 +1,47 @@
+﻿using LeBonCoinAPI.Models.EntityFramework;
+using LeBonCoinAPI.Models.Repository;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace LeBonCoinAPI.DataManager
+{
+    public class VilleManager : IRepositoryVille<Ville>
+    {
+        readonly DataContext? dataContext;
+        public VilleManager() { }
+        public VilleManager(DataContext context)
+        {
+            dataContext = context;
+        }
+        public ActionResult<IEnumerable<Ville>> GetAll()
+        {
+            return dataContext.Villes.ToList();
+        }
+
+        public ActionResult<Ville> GetByString(string codeInsee)
+        {
+            return dataContext.Villes.FirstOrDefault(u => u.CodeInsee.ToUpper() == codeInsee.ToUpper());
+        }
+        public void Add(Ville entity)
+        {
+            dataContext.Villes.Add(entity);
+            dataContext.SaveChanges();
+        }
+        public void Update(Ville ville, Ville entity)
+        {
+            dataContext.Entry(ville).State = EntityState.Modified;
+            ville.CodeInsee = entity.CodeInsee;
+            ville.DepartementCode = entity.DepartementCode;
+            ville.Nom = entity.Nom;
+            ville.CodePostal = entity.CodePostal; 
+
+            ville.AdressesVille = entity.AdressesVille;
+            dataContext.SaveChanges();
+        }
+        public void Delete(Ville ville)
+        {
+            dataContext.Villes.Remove(ville);
+            dataContext.SaveChanges();
+        }
+    }
+}
---
+++ LeBonCoinAPI/Models/Repository/IRepositoryVille.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LeBonCoinAPI.Models.Repository
+{
+    public interface IRepositoryVille<Tentity>
+    {
+        ActionResult<IEnumerable<Tentity>> GetAll();
+        ActionResult<Tentity> GetByString(string id);
+        void Add(Tentity entity);
+        void Update(Tentity entityToUpdate, Tentity entity);
+        void Delete(Tentity entity);
+    }
+}
