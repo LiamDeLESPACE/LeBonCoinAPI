c5aa0e72d80e1da62667b70a84043e41ef336040
Nicolas Tochon <Nicolas.Tochon@etu.univ-smb.fr>
2024-04-03 10:02:38 +0200

    repository manager commentaire (pas terminé)

---
+++ LeBonCoinAPI/DataManager/CommentaireManager.cs
@@ -0,0 +1,55 @@
+﻿using LeBonCoinAPI.Models.EntityFramework;
+using LeBonCoinAPI.Models.Repository;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace LeBonCoinAPI.DataManager
+{
+    public class CommentaireManager : IRepositoryCommentaire<Commentaire>
+    {
+        readonly DataContext? dataContext;
+        public CommentaireManager() { }
+        public CommentaireManager(DataContext context)
+        {
+            dataContext = context;
+        }
+        public ActionResult<IEnumerable<Commentaire>> GetAll()
+        {
+            return dataContext.Commentaires.ToList();
+        }
+
+        public ActionResult<Commentaire> GetByIds(int idReservation, int idProfil)
+        {
+            return dataContext.Commentaires.FirstOrDefault(c => c.ReservationId == idReservation && c.ProfilId == idProfil);
+        }
+
+        public ActionResult<Commentaire> GetByIdReservation(int idReservation)
+        {
+            return dataContext.Commentaires.FirstOrDefault(u => u.ReservationId == idReservation);
+        }
+
+        public ActionResult<Commentaire> GetByIdProfil(int idProfil)
+        {
+            return dataContext.Commentaires.FirstOrDefault(u => u.ProfilId == idProfil);
+        }
+        public void Add(Commentaire entity)
+        {
+            dataContext.Commentaires.Add(entity);
+            dataContext.SaveChanges();
+        }
+        public void Update(Commentaire commentaire, Commentaire entity)
+        {
+            dataContext.Entry(commentaire).State = EntityState.Modified;
+            commentaire.CarteId = entity.CarteId;
+            commentaire.ProfilId = entity.ProfilId;
+            commentaire.Numero = entity.Numero;
+
+            dataContext.SaveChanges();
+        }
+        public void Delete(Commentaire commentaire)
+        {
+            dataContext.Commentaires.Remove(commentaire);
+            dataContext.SaveChanges();
+        }
+    }
+}
