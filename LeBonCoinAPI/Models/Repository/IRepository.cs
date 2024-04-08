8e06d9150506a1f93b31500ebc0eda8164feb76c
AlixLejeune <alix.lejeune@etu.univ-smb.fr>
2024-04-08 15:08:46 +0200

    repo

---
+++ LeBonCoinAPI/Models/Repository/IRepository.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LeBonCoinAPI.Models.Repository
+{
+    public interface IRepository<TEntity>
+    {
+        Task<ActionResult<IEnumerable<TEntity>>> GetAll();
+        Task<ActionResult<TEntity>> GetById(int id);
+        Task Add(TEntity entity);
+        Task Update(TEntity entityToUpdate, TEntity entity);
+        Task Delete(TEntity entity);
+    }
+}
